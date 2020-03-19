d22f777b4adce7c81046269b723a84dffd6b28cb
fabfm4 <fabian.flores@benandfrank.com>
2020-03-19 01:19:50 -0600

    fix install application py

--- addons/install_application.py
+++ addons/install_application.py
@@ -6,19 +6,19 @@ from pprint import pprint
 
 json_file = open("application.json", "r")
 json_data = json.loads(json_file.read())
-if os.path.exists("./{}".format(json_data["main_project"])):
+if os.path.exists("./{0}".format(json_data["main_project"])):
     answer = raw_input('Enter your input [Y/n]:')
     if answer != "Y":
         print("OK!")
         #exit()
 else:
-    print("Cloning main repo ... {}".format(json_data["main_repo"]))
+    print("Cloning main repo ... {0}".format(json_data["main_repo"]))
     os.system("git clone {0} {1}".format(json_data["main_repo"], json_data["main_project"]))
     with  open("./{0}/{1}".format(json_data["main_project"], "oca_dependencies.txt")) as file_oca:
         line = file_oca.readline()
         while line:
             next_project, next_repo = line.split(" ")
-            print("Cloning ... {}".format(next_project))
+            next_repo = next_repo.strip()
             os.system("git clone {0} {1}".format(next_repo, next_project))
             line = file_oca.readline()
 
